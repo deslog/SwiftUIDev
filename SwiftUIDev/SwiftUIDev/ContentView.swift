6c98f46b71899e5a5447a63e9b957e99ca2866b3
JI SOO <dlwltn815@gmail.com>
2022-08-07 03:09:42 +0900

    [Chore] 프로젝트 생성

---
+++ SwiftUIDev/SwiftUIDev/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDev
+//
+//  Created by LeeJiSoo on 2022/08/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIDev/SwiftUIDev/SwiftUIDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDevApp.swift
+//  SwiftUIDev
+//
+//  Created by LeeJiSoo on 2022/08/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
